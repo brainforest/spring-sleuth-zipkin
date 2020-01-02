b98bfba4de408badaf0d8066e7ecb83bffbbfbde
gokcerbelgusen <gokcerb@gmail.com>
2020-01-02 23:41:54 +0300

    ..

--- micro-a/src/main/java/demo/MicroserviceA.java
+++ micro-a/src/main/java/demo/MicroserviceA.java
@@ -8,6 +8,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 /**
@@ -32,10 +36,20 @@ public class MicroserviceA implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws InterruptedException {
         LOG.info("invoking..");
-        String body =restTemplate.getForEntity("http://localhost:8070/invoke",String.class).getBody();
-        LOG.info(body);
+
+        final int a = 0;
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                String body =restTemplate.getForEntity("http://localhost:8070/invoke",String.class).getBody();
+            }
+        },0,1000L,TimeUnit.MILLISECONDS);
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+        executor.shutdown();
     }
 
 }
